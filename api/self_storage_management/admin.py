c8f97c1dad087e3e2cf42d85661b22cf0819288c
jscriptual <eddy@captaincode.cc>
2022-10-23 19:22:06 -0400

    adding customer and transaction models

--- api/self_storage_management/admin.py
+++ api/self_storage_management/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Unit
+from .models import Unit,Customer,Transaction
 
 # Register your models here.
 admin.site.register(Unit)
+admin.site.register(Customer)
+admin.site.register(Transaction)
--- api/self_storage_management/models.py
+++ api/self_storage_management/models.py
@@ -13,3 +13,23 @@ class Unit(models.Model):
         return f'Unit number: {self.unit_number}: Dimensions {self.dimension_width}, {self.dimension_length}, ' \
                f'{self.dimension_height} (width/length/height). Availability: {self.available}'
 
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    business_name = models.CharField(blank=True, max_length=40)
+    email_address = models.CharField(blank=True, max_length=50)
+    phone_number = models.TextField(
+        blank=True)  # TODO: add phone number validation (https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-a-phone-number-in-django-models)
+    street_address = models.CharField(blank=True, max_length=50)  # TODO: Add length validation
+    unit_access = models.ManyToManyField(Unit, related_name='customers', blank=True)
+
+    def __str__(self):
+        return f'Customer name: {self.first_name} {self.last_name}. \n Email: {self.email_address}\n Phone:{self.phone_number}\n' \
+               f' Access to unit: {self.unit_access}'
+
+
+class Transaction(models.Model):
+    date = models.DateTimeField()
+    unit_number = models.OneToOneField(Unit, on_delete=models.CASCADE)
+    payment_destination = models.CharField(max_length=20)
